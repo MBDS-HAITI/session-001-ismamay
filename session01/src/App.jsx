028848ede14833179bbbf937448dfea12baab68f
ismamay <ismamayy@yahoo.fr>
2025-12-01 21:25:55 -0500

    TD02: Afficher la date et l'heure du jour

--- session01/src/App.jsx
+++ session01/src/App.jsx
@@ -18,9 +18,19 @@ function Header() {
 }
 
 function MainContent() {
+    const now = new Date();
+    const day = new Intl.DateTimeFormat("fr-FR", { weekday: "long" }).format(now);
+    const month = new Intl.DateTimeFormat("fr-FR", { month: "long" }).format(now);
+    const year = now.getFullYear();
+    const time = new Intl.DateTimeFormat("fr-FR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    }).format(now);
+
     return (
         <main>
-            <p>Ici, nous afficherons des informations interessantes :</p>
+            <p>Bonjour, on est le {day} {now.getDate()}  {month} {year} et il est {time}</p>
         </main>    
     );
 }
@@ -45,7 +55,6 @@ function Footer() {
     );
 }
 function App() {
-  const [count, setCount] = useState(0)
 
     return (
         <>        
